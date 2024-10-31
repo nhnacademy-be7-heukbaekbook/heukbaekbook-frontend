92c23a83329bd4a329f4341020d3e249c15746ec
wjdtjdgns <person3275@gmail.com>
2024-10-31 18:55:13 +0900

    [Style] 불필요 import 제거

--- src/main/java/com/nhnacademy/heukbaekfrontend/member/client/LoginClient.java
+++ src/main/java/com/nhnacademy/heukbaekfrontend/member/client/LoginClient.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.heukbaekfrontend.member.client;
 
 import com.nhnacademy.heukbaekfrontend.member.dto.LoginRequest;
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
