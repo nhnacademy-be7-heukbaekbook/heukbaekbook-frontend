2944ed1b432330697353de8addf64a2de0c83eb3
daeminkim <kdm0068@gmail.com>
2024-11-05 14:41:51 +0900

    [Feat] 도서 관련 페이지 구현 - #14

--- src/main/java/com/nhnacademy/heukbaekfrontend/book/client/BookClient.java
+++ src/main/java/com/nhnacademy/heukbaekfrontend/book/client/BookClient.java
@@ -1,8 +1,11 @@
 package com.nhnacademy.heukbaekfrontend.book.client;
 
-import com.nhnacademy.heukbaekfrontend.book.dto.BookSummaryResponse;
+import com.nhnacademy.heukbaekfrontend.book.dto.response.BookDetailResponse;
+import com.nhnacademy.heukbaekfrontend.book.dto.response.BookSummaryResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -12,4 +15,7 @@ public interface BookClient {
 
     @GetMapping("/summary")
     List<BookSummaryResponse> getBooksSummary(@RequestParam List<Long> bookIds);
+
+    @GetMapping("/{bookId}")
+    ResponseEntity<BookDetailResponse> getBook(@PathVariable Long bookId);
 }
