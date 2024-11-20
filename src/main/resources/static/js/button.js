e7e70453e3fbef98c9842154b826106eccb0effd
김도현 <sjj2305@naver.com>
2024-11-20 23:37:18 +0900

    [검색] 카테고리 검색 구현 - #88

--- src/main/resources/static/js/button.js
+++ src/main/resources/static/js/button.js
@@ -46,27 +46,6 @@ function addToCart(bookId) {
         });
 }
 
-//
-// function sortBy(criteria) {
-//     // 현재 URL 가져오기
-//     const currentUrl = window.location.href;
-//
-//     // URL이 기본 경로일 경우 search 경로로 설정
-//     if (currentUrl === 'http://localhost:8080/' || currentUrl.endsWith('/')) {
-//         window.location.href = `/search?searchCondition=ALL&keyword=&sortCondition=${criteria}`;
-//     } else {
-//         // 현재 URL 객체 생성
-//         const updatedUrl = new URL(currentUrl);
-//
-//         // 기존 파라미터 업데이트 또는 추가
-//         updatedUrl.searchParams.set('sortCondition', criteria);
-//
-//         // URL 변경
-//         window.location.href = updatedUrl.toString();
-//     }
-//
-// }
-
 function maintainCategorySearch() {
     const urlParams = new URLSearchParams(window.location.search);
     const categoryId = urlParams.get('categoryId');
@@ -85,12 +64,11 @@ function handleEmptyKeyword() {
     const categoryId = document.getElementById("categoryId").value;
 
     if (!keywordInput && categoryId) {
-        // 키워드가 비어 있고 카테고리 ID가 있으면 `/search` 경로로 리다이렉트
         const sortCondition = new URLSearchParams(window.location.search).get("sortCondition") || "POPULARITY";
         window.location.href = `/search?searchCondition=ALL&keyword=&categoryId=${categoryId}&sortCondition=${sortCondition}`;
-        return false; // 기본 폼 제출 방지
+        return false;
     }
-    return true; // 키워드가 있을 경우 폼 제출 허용
+    return true;
 }
 
 function sortBy(criteria) {
@@ -99,7 +77,6 @@ function sortBy(criteria) {
     const searchCondition = urlParams.get("searchCondition") || "ALL";
     const categoryId = urlParams.get("categoryId") || "";
 
-    // `/search` 경로로 리다이렉트
     const searchUrl = `/search?searchCondition=${searchCondition}&keyword=${keyword}&categoryId=${categoryId}&sortCondition=${criteria}`;
     window.location.href = searchUrl;
 }
