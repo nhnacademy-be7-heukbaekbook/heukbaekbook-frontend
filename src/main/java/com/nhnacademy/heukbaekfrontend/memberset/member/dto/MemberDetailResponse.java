1db10baa0ee9ad17888994e5f9c8ca5d6316e7e0
jd-hyun <wjdehdgus3540@naver.com>
2024-11-29 15:30:27 +0900

    [Fix] 주문 폼 포인트 사용 적용 - #142

--- src/main/java/com/nhnacademy/heukbaekfrontend/memberset/member/dto/MemberDetailResponse.java
+++ src/main/java/com/nhnacademy/heukbaekfrontend/memberset/member/dto/MemberDetailResponse.java
@@ -2,11 +2,14 @@ package com.nhnacademy.heukbaekfrontend.memberset.member.dto;
 
 import com.nhnacademy.heukbaekfrontend.memberset.address.dto.MemberAddressResponse;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-public record MemberDetailResponse(Long id,
-                                   String customerName,
-                                   String customerPhoneNumber,
-                                   String customerEmail,
-                                   List<MemberAddressResponse> memberAddresses) {
+public record MemberDetailResponse(
+        Long id,
+        String customerName,
+        String customerPhoneNumber,
+        String customerEmail,
+        BigDecimal point,
+        List<MemberAddressResponse> memberAddresses) {
 }
